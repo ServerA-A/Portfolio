58d9ce185f6d0bd783730e0903b98456c3f60f7a
adityyaraj <92661311+adityyaraj@users.noreply.github.com>
2026-03-09 11:23:56 +0530

    fix: remove invalid tailwind options from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  content: ['./app/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  theme: {},
-  plugins:[],
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
